024456e911348207923fc9cb8aaf5bf94d69dccb
Piotr Malinowski <piotr.malinowski@monterail.com>
2022-12-16 14:21:18 +0100

    modify setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='onemind-metaplex-api',
-    version='',
-    packages=['api', 'metaplex'],
-    url='https://github.com/metaplex-foundation/python-api',
-    license='',
-    author=' Metaplex Foundation',
-    author_email='',
-    description='Process data from Solana / Metaplex'
-)
+setup()
