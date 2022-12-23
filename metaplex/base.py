e8f08bea6dfb171f11d1975c430aa0b3c3af711f
Piotr Malinowski <piotr.malinowski@monterail.com>
2022-12-23 18:47:09 +0100

    change way of pulling account

--- metaplex/base.py
+++ metaplex/base.py
@@ -12,9 +12,13 @@ class SolanaParser:
         return self.client.get_transaction(self.transaction_hash)
 
     @cached_property
-    def accounts(self):
-        return self.transactions.value.transaction.transaction.message.account_keys
+    def token_balances(self):
+        return self.transactions.value.transaction.meta.post_token_balances
 
     @cached_property
     def auction_account(self):
-        return self.accounts[3]  # auction account is 3rd in list
+        return [
+            o.owner for o in self.token_balances
+            if int(o.ui_token_amount.amount) > 0
+        ][0]
+
